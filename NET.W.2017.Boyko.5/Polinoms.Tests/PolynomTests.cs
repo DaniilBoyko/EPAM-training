e55c4470c3dea3631c4774f3416323dddd3d94da
Daniil Boyko <daniil.boyko.2017@gmail.com>
2017-10-28 00:54:14 +0300

    Day 5: Create class Polynom and PylynomTests

---
+++ NET.W.2017.Boyko.5/Polinoms.Tests/PolynomTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Polynoms.Tests
+{
+    [TestFixture]
+    public class PolynomTests
+    {
+
+
+        [TestCase(new double[] { 0, 23.2, 123.2 }, ExpectedResult = "23,2x + 123,2")]
+        [TestCase(new double[] { -1, -1, -1 }, ExpectedResult = "-x^2 - x - 1")]
+        [TestCase(new double[] { 1.2, 23.2, 123.2}, ExpectedResult = "1,2x^2 + 23,2x + 123,2")]
+        [TestCase(new double[] { 0, 0, 0 }, ExpectedResult = "0")]
+        [TestCase(new double[] { 0}, ExpectedResult = "0")]
+        [TestCase(new double[] { -1, 1, 1 }, ExpectedResult = "-x^2 + x + 1")]
+        [TestCase(new double[] { 1, 0, 1 }, ExpectedResult = "x^2 + 1")]
+        [TestCase(new double[] { 1, 0, 0, 1 }, ExpectedResult = "x^3 + 1")]
+        [TestCase(new double[] { -1, 0, 0, -1 }, ExpectedResult = "-x^3 - 1")]
+        [TestCase(new double[] { 1, 0, 0, 0 }, ExpectedResult = "x^3")]
+        [TestCase(null, ExpectedException = typeof(ArgumentNullException))]
+        [TestCase(new double[] { }, ExpectedException = typeof(ArgumentException))]
+        public string ToStringMethod_ConvertPolinomialToString(double[] coefficients)
+        {
+            return new Polynom(coefficients).ToString();
+        }
+
+        [TestCase(new double[] { 0, 23.2, 123.2 }, new double[] { 0, 23.2, 123.2 }, ExpectedResult = true)]
+        [TestCase(new double[] { -1, -1, -1 }, new double[] { -1, -1, 1 }, ExpectedResult = false)]
+        [TestCase(new double[] { 1.2, 23.2, 123.2 }, new double[] { 1.2, 23.2, 123.2, 0 }, ExpectedResult = false)]
+        [TestCase(new double[] { 0, 0, 0 }, new double[] { 0 }, ExpectedResult = true)]
+        public bool EqualsMethod_CheckIfObjecstIsEqual(double[] coefficients1, double[] coefficients2)
+        {
+            return new Polynom(coefficients1).Equals(new Polynom(coefficients2));
+        }
+
+        [TestCase(new double[] { 0, 23.2, 123.2 }, new double[] { 0, 23.2, 123.2 }, ExpectedResult = true)]
+        [TestCase(new double[] { -1, -1, -1 }, new double[] { -1, -1, -1 }, ExpectedResult = true)]
+        [TestCase(new double[] { 0, 0, 0 }, new double[] { 0 }, ExpectedResult = true)]
+        public bool GetHashCodeMethod_CheckHashCodeOfTwoEqualPolinomial(double[] coefficients1, double[] coefficients2)
+        {
+            return new Polynom(coefficients1).GetHashCode() == new Polynom(coefficients2).GetHashCode();
+        }
+
+        [TestCase(new double[] { 0, 23.2, 123.2 }, 10, ExpectedResult = "232x + 1232")]
+        [TestCase(new double[] { -1, -1, -1 }, 1, ExpectedResult = "-x^2 - x - 1")]
+        [TestCase(new double[] { 1.2, 23.2, 123.2 }, -1, ExpectedResult = "-1,2x^2 - 23,2x - 123,2")]
+        [TestCase(new double[] { 0, 0, 0 }, 100, ExpectedResult = "0")]
+        [TestCase(new double[] { -1, 1, 1 }, 0, ExpectedResult = "0")]
+        public string MultiplicationOperatorPolynomNumber_CheckMultiplicationOperatorBetweenPlinomAndDouble(double[] coefficients, double value)
+        {
+            return (new Polynom(coefficients) * value).ToString();
+        }
+
+        [TestCase(new double[] { 0, 23.2, 123.2 }, new double[] { 0, 23.2, 123.2 }, ExpectedResult = "46,4x + 246,4")]
+        [TestCase(new double[] { -1, -1, -1 }, new double[] { -1, 1, 1 }, ExpectedResult = "-2x^2")]
+        [TestCase(new double[] { 1.2, 23.2, 123.2 }, new double[] { 0, 0, 0 }, ExpectedResult = "1,2x^2 + 23,2x + 123,2")]
+        [TestCase(new double[] { 0, 0, 0 }, new double[] { -11, 0, 1 }, ExpectedResult = "-11x^2 + 1")]
+        [TestCase(new double[] { -1, 1, 1 }, new double[] { -1, 1, 1 }, ExpectedResult = "-2x^2 + 2x + 2")]
+        [TestCase(new double[] { -1, 1, 1 }, new double[] { -1 }, ExpectedResult = "-x^2 + x")]
+        [TestCase(new double[] { -1 }, new double[] { -1, 1, 1 }, ExpectedResult = "-x^2 + x")]
+        public string SumOperator_CheckSummationOfTwoPolinoms(double[] coefficients1, double[] coefficients2)
+        {
+            return (new Polynom(coefficients1) + new Polynom(coefficients2)).ToString();
+        }
+
+        [TestCase(new double[] { 0, 23.2, 123.2 }, 1, ExpectedResult = 146.4)]
+        [TestCase(new double[] { -1, -1, -1 }, 1, ExpectedResult = -3)]
+        [TestCase(new double[] { 1.2, 23.2, 123.2 }, 10, ExpectedResult = 475.2)]
+        [TestCase(new double[] { 0, 0, 0 }, 100, ExpectedResult = 0)]
+        [TestCase(new double[] { -1, 1, 1 }, 5, ExpectedResult = -19)]
+        public double EvaluateMethod_CalculatePolynomWithSetValue(double[] coefficients, double value)
+        {
+            return new Polynom(coefficients).Evaluate(value);
+        }
+
+        [TestCase(new double[] { -1, -1, -1 }, new double[] { -1, 1, 1 }, ExpectedResult = "x^4 - x^2 - 2x - 1")]
+        [TestCase(new double[] { 1.2, 23.2, 123.2 }, new double[] { 0, 0, 0 }, ExpectedResult = "0")]
+        [TestCase(new double[] { 0, 0, 0 }, new double[] { -11, 0, 1 }, ExpectedResult = "0")]
+        [TestCase(new double[] { -1 }, new double[] { -1, 1, 1 }, ExpectedResult = "x^2 - x - 1")]
+        [TestCase(new double[] { 1, -1 }, new double[] { 1, 1 }, ExpectedResult = "x^2 - 1")]
+        public string MultiplicationOperatorPolynomPolynom_MultiplicationOperatorBetweenTwoPolynoms(
+            double[] coefficients1, double[] coefficients2)
+        {
+            return (new Polynom(coefficients1) * new Polynom(coefficients2)).ToString();
+        }
+    }
+}
---
+++ NET.W.2017.Boyko.5/Polinoms/Polynom.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Polynoms
+{
+    public class Polynom : ICloneable
+    {
+        #region Private Fields
+
+        private double[] _coefficients;
+
+        private char Letter { get; set; }
+        private double[] Coefficients
+        {
+            get => _coefficients;
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException(nameof(value));
+
+                if (value.Length == 0)
+                    throw new ArgumentException($"{nameof(value)} should contains 1 and more values", nameof(value));
+
+                int countZeros = 0;
+                while (countZeros < value.Length && value[countZeros] == 0)
+                    countZeros++;
+                double[] coefs;
+
+                if (countZeros == value.Length)
+                {
+                    _coefficients = new double[] { 0 };
+                    return;
+                }
+
+                coefs = new double[value.Length - countZeros];
+                for (int i = countZeros, j = 0; i < value.Length; i++, j++)
+                    coefs[j] = value[i];
+
+                _coefficients = coefs;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        private Polynom(char letter, params double[] coefficients)
+        {
+            Letter = letter;
+            Coefficients = coefficients;
+        }
+
+        public Polynom(params double[] coefficients) : this('x', coefficients) { }
+
+        #endregion
+
+        #region Override Methods
+
+        public override string ToString()
+        {
+            if (Coefficients.Length == 1) return Coefficients[0].ToString(CultureInfo.InvariantCulture);
+
+            int n = Coefficients.Length - 1;
+            var resultStr = new StringBuilder();
+
+            for (int i = n, j = 0; i >= 0; i--, j++)
+            {
+                //Skip if zero
+                if (Coefficients[j] == 0)
+                    continue;
+
+                //Print sign
+                if (Coefficients[j] > 0 && i != n) resultStr.Append(" + ");
+                if (Coefficients[j] < 0)
+                {
+                    resultStr.Append(i == n ? "-" : " - ");
+                }
+
+                //Print value
+                if (Math.Abs(Coefficients[j]) != 1 || i == 0)
+                    resultStr.Append(Math.Abs(Coefficients[j]));
+
+                //Print letter
+                if (i != 0)
+                resultStr.Append(Letter);
+
+                //Print power
+                if (i > 1)
+                    resultStr.Append("^" + i);
+            }
+
+            return resultStr.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Polynom polynomial = (Polynom)obj;
+
+            if (Coefficients.Length != polynomial.Coefficients.Length)
+                return false;
+
+            for (int i = 0; i < Coefficients.Length; i++)
+                if (Coefficients[i] != polynomial.Coefficients[i])
+                    return false;
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = Coefficients[0].GetHashCode();
+            for (int i = 1; i < Coefficients.Length; i++)
+            {
+                hashCode ^= Coefficients[i].GetHashCode();
+            }
+            return hashCode;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public static bool Equals(Polynom polynom1, Polynom polynom2)
+        {
+            if (polynom1 == polynom2) return true;
+            if (polynom1 == null || polynom2 == null)
+                return false;
+            return polynom1.Equals(polynom2);
+        }
+
+        public object Clone()
+        {
+            return new Polynom(Letter, Coefficients);
+        }
+
+        public double Evaluate(double xValue)
+        {
+            double summ = Coefficients[Coefficients.Length - 1];
+            for (int i = Coefficients.Length - 2; i >= 0; i--)
+            {
+                summ += Coefficients[i] * xValue;
+                xValue *= xValue;
+            }
+            return summ;
+        }
+
+        #endregion
+
+        #region Operators
+
+        public static Polynom operator -(Polynom polynom)
+        {
+            return polynom * (-1);
+        }
+
+        public static Polynom operator +(Polynom polynom)
+        {
+            return (Polynom)polynom.Clone();
+        }
+
+        public static Polynom operator +(Polynom polynom, double value)
+        {
+            return polynom + new Polynom(value);
+        }
+
+        public static Polynom operator +(double value, Polynom polynom)
+        {
+            return polynom + value;
+        }
+
+        public static Polynom operator -(Polynom polynom, double value)
+        {
+            return polynom + (-value);
+        }
+
+        public static Polynom operator -(double value, Polynom polynom)
+        {
+            return value + (-polynom);
+        }
+
+        public static Polynom operator *(Polynom polynom, double value)
+        {
+            double[] resultCoefs = (double[])polynom.Coefficients.Clone();
+
+            for (int i = 0; i < resultCoefs.Length; i++)
+                resultCoefs[i] *= value;
+
+            return new Polynom(resultCoefs);
+        }
+
+        public static Polynom operator *(double value, Polynom polynom)
+        {
+            return polynom * value;
+        }
+
+        public static Polynom operator /(Polynom polynom, double value)
+        {
+            return polynom * (1 / value);
+        }
+
+        public static Polynom operator +(Polynom polynom1, Polynom polynom2)
+        {
+            double[] minCoefs, maxCoefs;
+            SetMaxpolynomCoefficients(polynom1, polynom2, out maxCoefs, out minCoefs);
+
+            double[] resultCefs = (double[])maxCoefs.Clone();
+            for (int i = resultCefs.Length - 1, j = minCoefs.Length - 1; i >= 0 && j >= 0; i--, j--)
+                resultCefs[i] += minCoefs[j];
+
+            return new Polynom(resultCefs);
+        }
+
+        public static Polynom operator *(Polynom polynom1, Polynom polynom2)
+        {
+            double[] coefs1 = polynom1.Coefficients, coefs2 = polynom2.Coefficients;
+            double[] resultCoefs = new double[coefs1.Length + coefs2.Length - 1];
+
+            for (int i = 0; i < coefs1.Length; i++)
+                for (int j = 0; j < coefs2.Length; j++)
+                    resultCoefs[i + j] += coefs1[i] * coefs2[j];
+
+            return new Polynom(resultCoefs);
+        }
+
+        public static Polynom operator -(Polynom polynom1, Polynom polynom2)
+        {
+            return polynom1 + (-polynom2);
+        }
+
+        public static bool operator ==(Polynom polynom1, Polynom polynom2)
+        {
+            return Equals(polynom1, polynom2);
+        }
+
+        public static bool operator !=(Polynom polynom1, Polynom polynom2)
+        {
+            return !(polynom1 == polynom2);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static void SetMaxpolynomCoefficients(Polynom polynom1, Polynom polynom2, out double[] maxCoefs,
+            out double[] minCoefs)
+        {
+            if (polynom1.Coefficients.Length > polynom2.Coefficients.Length)
+            {
+                maxCoefs = polynom1.Coefficients;
+                minCoefs = polynom2.Coefficients;
+            }
+            else
+            {
+                maxCoefs = polynom2.Coefficients;
+                minCoefs = polynom1.Coefficients;
+            }
+        }
+
+        #endregion
+    }
+}
