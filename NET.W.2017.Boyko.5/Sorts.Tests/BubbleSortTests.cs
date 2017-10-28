c6c81f798ba0d208ad9583d7727a7d85dfec8d23
Daniil Boyko <daniil.boyko.2017@gmail.com>
2017-10-28 12:52:18 +0300

    Day 5: Create class BubbleSort and BubbleSortTests

---
+++ NET.W.2017.Boyko.5/Sorts.Tests/BubbleSortTests.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Sorts.Tests
+{
+    [TestFixture]
+    public class BubbleSortTests
+    {
+
+        public static object[] CaseSourceTestDataIncreaseByMax =
+        {
+            new object[] 
+            {
+                new double[][]
+                    {
+                        new double[] {2.3, 12.31, -1.0},
+                        new double[] {123.39, 12.0},
+                        new double[] {1.0, 10.0, 10.0},
+                    },
+                new double[][]   {
+                    new double[] {1.0, 10.0, 10.0},
+                    new double[] {2.3, 12.31, -1.0},
+                    new double[] {123.39, 12.0},
+                    
+                },
+            },
+
+            new object[]
+            {
+                new double[][]
+                {
+                    new double[] {-1},
+                    new double[] {3.3456, 128},
+                    new double[] {2, -40, 256},
+                },
+                new double[][]   {
+                    new double[] {-1},
+                    new double[] {3.3456, 128},
+                    new double[] {2, -40, 256},
+                },
+            },
+        };
+
+        public static object[] CaseSourceTestDataDecreaseByMax =
+        {
+            new object[]
+            {
+                new double[][]
+                {
+                    new double[] {2.3, 12.31, -1.0},
+                    new double[] {123.39, 12.0},
+                    new double[] {1.0, 10.0, 10.0},
+                },
+                new double[][]   {
+                    new double[] {123.39, 12.0},
+                    new double[] {2.3, 12.31, -1.0},
+                    new double[] {1.0, 10.0, 10.0},
+                },
+            },
+
+            new object[]
+            {
+                new double[][]
+                {
+                    new double[] {-1},
+                    new double[] {3.3456, 128},
+                    new double[] {2, -40, 256},
+                },
+                new double[][]   {
+                    new double[] {2, -40, 256},
+                    new double[] {3.3456, 128},
+                    new double[] {-1},
+                },
+            },
+        };
+
+        public static object[] CaseSourceTestDataDecreaseByMin =
+        {
+            new object[]
+            {
+                new double[][]
+                {
+                    new double[] {2.3, 12.31, -1.0},
+                    new double[] {123.39, 12.0},
+                    new double[] {1.0, 10.0, 10.0},
+                },
+                new double[][]   {
+                    new double[] {123.39, 12.0},
+                    new double[] {1.0, 10.0, 10.0},
+                    new double[] {2.3, 12.31, -1.0},
+                },
+            },
+
+            new object[]
+            {
+                new double[][]
+                {
+                    new double[] {-1},
+                    new double[] {3.3456, 128},
+                    new double[] {2, -40, 256},
+                },
+                new double[][]   {
+                    new double[] {3.3456, 128},
+                    new double[] {-1},
+                    new double[] {2, -40, 256},
+                },
+            },
+        };
+
+        public static object[] CaseSourceTestDataIncreaseBySum =
+        {
+            new object[]
+            {
+                new double[][]
+                {
+                    new double[] {2.3, 12.31, -1.0},
+                    new double[] {123.39, 12.0},
+                    new double[] {1.0, 10.0, 10.0},
+                },
+                new double[][]   {
+                    new double[] {2.3, 12.31, -1.0},
+                    new double[] {1.0, 10.0, 10.0},
+                    new double[] {123.39, 12.0},
+                },
+            },
+
+            new object[]
+            {
+                new double[][]
+                {
+                    new double[] {-1},
+                    new double[] {3.3456, 128},
+                    new double[] {2, -40, 256},
+                },
+                new double[][]   {
+                    new double[] {-1},
+                    new double[] {3.3456, 128},
+                    new double[] {2, -40, 256},
+                },
+            },
+        };
+
+        [Test, TestCaseSource(nameof(CaseSourceTestDataIncreaseByMax))]
+        public void SortJaggedArrayMethod_SortByIncreaseMaxElements(double[][] array, double[][] resultArray)
+        {
+            BubbleSort.SortJaggedArray(array, BubbleSort.MaxNumber, new BubbleSort.Increase());
+            Assert.That(array, Is.EqualTo(resultArray));
+        }
+
+        [Test, TestCaseSource(nameof(CaseSourceTestDataDecreaseByMax))]
+        public void SortJaggedArrayMethod_SortByDecreaseMaxElements(double[][] array, double[][] resultArray)
+        {
+            BubbleSort.SortJaggedArray(array, BubbleSort.MaxNumber, new BubbleSort.Decrease());
+            Assert.That(array, Is.EqualTo(resultArray));
+        }
+
+        [Test, TestCaseSource(nameof(CaseSourceTestDataDecreaseByMin))]
+        public void SortJaggedArrayMethod_SortByDecreaseMinElements(double[][] array, double[][] resultArray)
+        {
+            BubbleSort.SortJaggedArray(array, BubbleSort.MinNumber, new BubbleSort.Decrease());
+            Assert.That(array, Is.EqualTo(resultArray));
+        }
+
+        [Test, TestCaseSource(nameof(CaseSourceTestDataIncreaseBySum))]
+        public void SortJaggedArrayMethod_SortByIncreaseSumElements(double[][] array, double[][] resultArray)
+        {
+            BubbleSort.SortJaggedArray(array, BubbleSort.Sum, new BubbleSort.Increase());
+            Assert.That(array, Is.EqualTo(resultArray));
+        }
+    }
+}
---
+++ NET.W.2017.Boyko.5/Sorts/BubbleSort.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorts
+{
+    public class BubbleSort
+    {
+        #region Public Methods
+
+        public static double Sum(double[] array)
+        {
+            CheckArray(array);
+
+            double sum = 0;
+            foreach (var number in array)
+                sum += number;
+            return sum;
+        }
+
+        public static double MaxNumber(double[] array)
+        {
+            CheckArray(array);
+
+            double max = array[0];
+            foreach (var number in array)
+                if (max < number)
+                    max = number;
+            return max;
+        }
+
+        public static double MinNumber(double[] array)
+        {
+            CheckArray(array);
+
+            double min = array[0];
+            foreach (var number in array)
+                if (min > number)
+                    min = number;
+            return min;
+        }
+
+        public class Decrease : IComparer<double>
+        {
+            public int Compare(double x, double y)
+            {
+                if (x > y) return 1;
+                if (x < y) return -1;
+                return 0;
+            }
+        }
+
+        public class Increase : IComparer<double>
+        {
+            public int Compare(double x, double y)
+            {
+                if (x < y) return 1;
+                if (x > y) return -1;
+                return 0;
+            }
+        }
+
+        public static void SortJaggedArray(double[][] array, Func<double[], double> calcRow, IComparer<double> comparable)
+        {
+            CheckArray(array);
+
+            double[] rowsElements = new double[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                rowsElements[i] = calcRow(array[i]);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = array.Length - 1; j > i; j--)
+                {
+                    if (comparable.Compare(rowsElements[j - 1], rowsElements[j]) == -1)
+                    {
+                        Swap(ref rowsElements[j - 1], ref rowsElements[j]);
+                        Swap(ref array[j - 1], ref array[j]);
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static bool CheckArray(Array array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+
+            if (array.Length == 0)
+                throw new ArgumentException($"{nameof(array)} should contains 1 or more elements", nameof(array));
+
+            return true;
+        }
+
+        private static void Swap<T>(ref T a, ref T b)
+        {
+            T c = a;
+            a = b;
+            b = c;
+        }
+
+        #endregion  
+    }
+}
