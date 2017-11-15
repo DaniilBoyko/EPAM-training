36dcba313cf4252a7ade4ab10b8fb0eef9ae7fc0
Daniil Boyko <daniil.boyko.2017@gmail.com>
2017-11-15 19:50:25 +0300

    Day 12: Created Fibonacci and tests

---
+++ NET.W.2017.Boyko.12/Fibonacci.Tests/FibonacciTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Fibonacci.Tests
+{
+    [TestFixture]
+    public class FibonacciTests
+    {
+        [TestCase(0, ExpectedException = typeof(ArgumentOutOfRangeException))]
+        [TestCase(-1, ExpectedException = typeof(ArgumentOutOfRangeException))]
+        [TestCase(1, ExpectedResult = 1)]
+        [TestCase(2, ExpectedResult = 1)]
+        [TestCase(3, ExpectedResult = 2)]
+        [TestCase(4, ExpectedResult = 3)]
+        [TestCase(5, ExpectedResult = 5)]
+        [TestCase(6, ExpectedResult = 8)]
+        public long GetNumberTests_GetFibonacciNumberAtIndex(int index)
+        {
+            return Fibonacci.GetNumber(index);
+        }
+
+        [TestCase(0, ExpectedException = typeof(ArgumentOutOfRangeException))]
+        [TestCase(-1, ExpectedException = typeof(ArgumentOutOfRangeException))]
+        [TestCase(1, ExpectedResult = new long[] { 1 })]
+        [TestCase(2, ExpectedResult = new long[] { 1, 1 })]
+        [TestCase(3, ExpectedResult = new long[] { 1, 1, 2})]
+        [TestCase(4, ExpectedResult = new long[] { 1, 1, 2, 3})]
+        [TestCase(5, ExpectedResult = new long[] { 1, 1, 2, 3, 5 })]
+        [TestCase(6, ExpectedResult = new long[] { 1, 1, 2, 3, 5, 8 })]
+        public long[] GetNumberTests_GetFibonacciNumbersSequence(int index)
+        {
+            return Fibonacci.GetSequence(index);
+        }
+    }
+}
---
+++ NET.W.2017.Boyko.12/Fibonacci/Fibonacci.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public static class Fibonacci
+    {
+        public static long GetNumber(int index)
+        {
+            if (index <= 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(index)} should be more then 0");
+            }
+
+            if (index == 1 || index == 2)
+            {
+                return 1;
+            }
+
+            long[] prevNumbers = new long[] {1, 1};
+
+            for (int i = 2; i < index; i++)
+            {
+                long temp = prevNumbers[1];
+                prevNumbers[1] = prevNumbers[0] + prevNumbers[1];
+                prevNumbers[0] = temp;
+            }
+
+            return prevNumbers[1];
+        }
+
+        public static long[] GetSequence(int index)
+        {
+            if (index <= 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(index)} should be more then 0");
+            }
+
+            if (index == 1)
+            {
+                return new long[] {1};
+            }
+
+            if (index == 2)
+            {
+                return new long[] {1, 1};
+            }
+
+            long[] numbers = new long[index];
+            numbers[0] = 1;
+            numbers[1] = 1;
+            for (int i = 2; i < index; i++)
+            {
+                numbers[i] = numbers[i - 1] + numbers[i - 2];
+            }
+
+            return numbers;
+        }
+    }
+}
