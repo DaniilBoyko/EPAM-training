fd87e0cc7b0c0f2065daa1aed855d320579bb2eb
Daniil Boyko <daniil.boyko.2017@gmail.com>
2017-11-28 09:29:40 +0300

    Test: fix Task1

--- NET.W.2017.Boyko.Test/Task1.Console/Program.cs
+++ NET.W.2017.Boyko.Test/Task1.Console/Program.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task1.Solution;
 
 
 namespace Task1.Console
 {
     class Program
     {
+
+        public static CheckCondition CreateOwnCheckCondition()
+        {
+            CheckCondition first = new CheckCondition(pas => pas == null, pas => throw new ArgumentException($"{pas} is null arg"));
+            CheckCondition second = new CheckCondition(pas => pas == string.Empty, pas => Tuple.Create(false, $"{pas} is empty "));
+            first.SetNextCheckCondition(second);
+            second.SetNextCheckCondition(new CheckCondition(pas => pas.Length <= 7, pas => Tuple.Create(false, $"{pas} length too short")));
+
+            return first;
+        }
+
         public static void ShowInfo(Tuple<bool, string> tuple)
         {
             System.Console.WriteLine("Password {0} is {1}/n", tuple.Item1, tuple.Item2);
@@ -20,19 +32,13 @@ namespace Task1.Console
             PasswordCheckerService passwordCheckerService = new PasswordCheckerService(new SqlRepository());
             try
             {
-                ShowInfo(passwordCheckerService.VerifyPassword(null));
+                ShowInfo(passwordCheckerService.VerifyPassword(CreateOwnCheckCondition(), string.Empty));
             }
             catch (Exception e)
             {
                 System.Console.WriteLine(e.Message);
             }
 
-            ShowInfo(passwordCheckerService.VerifyPassword(String.Empty));
-            ShowInfo(passwordCheckerService.VerifyPassword("123456"));
-            ShowInfo(passwordCheckerService.VerifyPassword("123456789012345678"));
-            ShowInfo(passwordCheckerService.VerifyPassword("12345678"));
-            ShowInfo(passwordCheckerService.VerifyPassword("abcdefasd"));
-            ShowInfo(passwordCheckerService.VerifyPassword("abcdefasd1"));
         }
     }
 }
