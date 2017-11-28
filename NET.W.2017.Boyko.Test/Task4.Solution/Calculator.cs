a798f4b82226095144c2d421fa8fa3d810c790fe
Daniil Boyko <daniil.boyko.2017@gmail.com>
2017-11-28 10:39:47 +0300

    Test: Task 4 completed

---
+++ NET.W.2017.Boyko.Test/Task4.Solution/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task4.Solution
+{
+    public class Calculator
+    {
+        public double CalculateAverage(IEnumerable<double> values, IAveragingMethod averagingMethod)
+        {
+            Check(values, averagingMethod);
+            return averagingMethod.Calculate(values);
+        }
+
+        public double CalculateAverage(IEnumerable<double> values, Func<IEnumerable<double>, double> calculateFunc)
+        {
+            Check(values, calculateFunc);
+            return calculateFunc(values);
+        }
+
+        private void Check(IEnumerable<double> values, IAveragingMethod averagingMethod)
+        {
+            if (values == null)
+                throw new ArgumentNullException(nameof(values));
+
+            if (averagingMethod == null)
+                throw new ArgumentNullException(nameof(averagingMethod));
+        }
+
+        private void Check(IEnumerable<double> values, Func<IEnumerable<double>, double> calculateFunc)
+        {
+            if (values == null)
+                throw new ArgumentNullException(nameof(values));
+
+            if (calculateFunc == null)
+                throw new ArgumentNullException(nameof(calculateFunc));
+        }
+    }
+}
---
+++ NET.W.2017.Boyko.Test/Task4.Solution/Median.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.Solution
+{
+    public class Median : IAveragingMethod
+    {
+        public double Calculate(IEnumerable<double> elements)
+        {
+            var sortedValues = elements.OrderBy(x => x).ToList();
+
+            int n = sortedValues.Count;
+
+            if (n % 2 == 1)
+            {
+                return sortedValues[(n - 1) / 2];
+            }
+
+            return (sortedValues[sortedValues.Count / 2 - 1] + sortedValues[n / 2]) / 2;
+        }
+    }
+}
