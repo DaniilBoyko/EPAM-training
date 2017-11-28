7a305f9aeb6d64d446317445d2932cb9b34c3d74
Daniil Boyko <daniil.boyko.2017@gmail.com>
2017-11-28 08:33:12 +0300

    Day 16: start template

---
+++ NET.W.2017.Boyko.16/URL-Parser/Interfaces/Parser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace URL_Parser.Interfaces
+{
+    public abstract class Parser
+    {
+        Parser NextParser { get; set; }
+
+        public abstract void Parse(string info, XElement element);
+
+        private void CallNextParser(string info, XElement element)
+        {
+            NextParser?.Parse(info, element);
+        }
+    }
+}
---
+++ NET.W.2017.Boyko.16/URL-Parser/Parsers/SchemeParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using URL_Parser.Interfaces;
+
+namespace URL_Parser.Parsers
+{
+    public class SchemeParser : Parser
+    {
+        public void Parse(string info, XElement element)
+        {
+            
+        }
+    }
+}
