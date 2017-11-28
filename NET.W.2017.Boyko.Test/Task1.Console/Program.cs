84a127bbf5ba29665715c0a6f161ff9eb68095d0
Daniil Boyko <daniil.boyko.2017@gmail.com>
2017-11-28 08:52:11 +0300

    Test: Task1 completed

--- NET.W.2017.Boyko.Test/Task1.Console/Program.cs
+++ NET.W.2017.Boyko.Test/Task1.Console/Program.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Task1.Console
 {
     class Program
     {
+        public static void ShowInfo(Tuple<bool, string> tuple)
+        {
+            System.Console.WriteLine("Password {0} is {1}/n", tuple.Item1, tuple.Item2);
+        }
+
         static void Main(string[] args)
         {
+          
+            PasswordCheckerService passwordCheckerService = new PasswordCheckerService(new SqlRepository());
+            try
+            {
+                ShowInfo(passwordCheckerService.VerifyPassword(null));
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+
+            ShowInfo(passwordCheckerService.VerifyPassword(String.Empty));
+            ShowInfo(passwordCheckerService.VerifyPassword("123456"));
+            ShowInfo(passwordCheckerService.VerifyPassword("123456789012345678"));
+            ShowInfo(passwordCheckerService.VerifyPassword("12345678"));
+            ShowInfo(passwordCheckerService.VerifyPassword("abcdefasd"));
+            ShowInfo(passwordCheckerService.VerifyPassword("abcdefasd1"));
         }
     }
 }
